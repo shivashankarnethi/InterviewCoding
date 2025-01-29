d72d0d6b5b013aa9088107c1c91716e41f8647e3
Shiva Shankar <shivashankarnethi@gmail.com>
2025-01-29 12:59:21 +0530

    Amazonpaycommit

---
+++ Java8Coding/src/com/Shiva/Java8/AmazonPay.java
@@ -0,0 +1,18 @@
+package com.Shiva.Java8;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AmazonPay {
+
+	public static void main(String[] args) {
+		Map<String,Integer> map=new HashMap<String, Integer>();
+		  map.put(null,123);
+		  map.put("chandu",456);
+		  map.put(null,123);
+		  map.put(null,456);
+		  System.out.println(map);
+
+	}
+
+}
